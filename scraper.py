d333cdf813aa3db35cba4c5364ca27b299d0cb91
Lsdbot <s_levsh@bk.ru>
2021-06-23 22:36:33 +0300

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://geogoroda.ru/search/node/Алаберды"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "lxml")
+def CheckCityOrNot(cityOfPlayer):
+    url = "https://geogoroda.ru/search/node/"+cityOfPlayer
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+    return soup.find_all("h2")[1].text
 
 
-print(soup)
+
