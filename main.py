872658bcd83b8f63b3fa1b2eeef3611a3701858c
Lsdbot <s_levsh@bk.ru>
2021-07-13 13:06:56 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import Design
 import Logic
 import shelve
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets,QtCore
 
 class Application(QtWidgets.QMainWindow, Design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.inputButton.clicked.connect(lambda: self.Game(self.inputLine.text()))
+        self.helpButton.clicked.connect(lambda: self.Help())
         self.counter = True
         self.chosedCity = ""
+
+    def Help(self):
+        self.labelProgramme.setText("Пошел нахер, загугли")
+
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key.Key_Return:
+            self.Game(self.inputLine.text())
+        e.accept()
 
     def Game(self, City):
         if self.counter:
